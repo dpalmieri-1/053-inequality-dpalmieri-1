0e80036be29572e12da1353d9ad2ee9bda44f83c
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-16 18:01:58 +0000

    Undefined - final result

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function guessNumber(guess){
+    if (guess != 25){
+        document.getElementById('result').innerHTML = 'not it';
+    }
+}
+function strictGuessNumber(guess){
+    if (guess !== 25){
+        document.getElementById('result').innerHTML += 'not it';
+    }
+}
 
+document.getElementById('result').innerHTML = strictGuessNumber('25');
