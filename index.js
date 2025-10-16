6d69d6bcdac41688affc5c4497a4274910865370
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-16 18:06:58 +0000

    Fixed it.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function guessNumber(guess){
     if (guess != 25){
-        document.getElementById('result').innerHTML = 'not it';
+        return 'not it';
     }
 }
 function strictGuessNumber(guess){
     if (guess !== 25){
-        document.getElementById('result').innerHTML += 'not it';
+        return 'not it';
     }
 }
 
-document.getElementById('result').innerHTML = strictGuessNumber('25');
+document.getElementById('result').innerHTML = strictGuessNumber(24);
+document.getElementById('result').innerHTML += guessNumber(24);
